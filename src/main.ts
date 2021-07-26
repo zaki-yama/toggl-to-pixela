c73894c8c87c0fbcaecd3fab2f2a5892c96bd579
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-07-26 12:10:05 +0900

    chore: try Pixe.la API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import fetch from "node-fetch";
 
 const TOGGL_URL = "https://api.track.toggl.com/api/v8/time_entries";
 
+const PIXELA_USERNAME = "zaki-yama";
+const PIXELA_USER_TOKEN = process.env.PIXELA_TOKEN;
+
 // eslint-disable-next-line node/no-unsupported-features/node-builtins
 const params = new URLSearchParams({
   start_date: "2021-07-23T00:00:00+09:00",
@@ -21,3 +24,20 @@ const response = await fetch(`${TOGGL_URL}?${params.toString()}`, {
 });
 const timeEntries: TimeEntry[] = await response.json();
 console.log(timeEntries);
+
+const graphId = "playground";
+const pixel = await fetch(
+  `https://pixe.la/v1/users/${PIXELA_USERNAME}/graphs/${graphId}`,
+  {
+    method: "POST",
+    headers: {
+      "X-USER-TOKEN": `${PIXELA_USER_TOKEN}`,
+    },
+    body: JSON.stringify({
+      date: "20210718",
+      quantity: "25",
+    }),
+  }
+);
+console.log(pixel.status);
+console.log(await pixel.json());
